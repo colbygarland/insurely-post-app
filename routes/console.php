1f2bf16b2452e7f812b01b6cfd8a69d3803fbfce
Colby Garland <colbygarland@gmail.com>
2025-02-21 12:25:47 -0700

    run

--- routes/console.php
+++ routes/console.php
@@ -5,9 +5,13 @@ use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('telescope:prune --hours=48')->daily();
 
-// Schedule::call(function () {
-//     // Get the new posts from Wordpress 
-//     Post::fetchFromWordpress();
-//     // Post to LinkedIn
-//     Post::postToLinkedIn();
-// })->daily();
+Schedule::call(function () {
+    $run = env('SCHEDULE_POSTS');
+
+    if($run){
+        // Get the new posts from Wordpress 
+        Post::fetchFromWordpress();
+        // Post to LinkedIn
+        Post::postToLinkedIn();
+    }
+})->daily();
