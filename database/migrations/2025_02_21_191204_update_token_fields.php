893eeb6576c91ea7ab5a31e2cf308d90fa85d331
Colby Garland <colbygarland@gmail.com>
2025-02-21 12:12:50 -0700

    adds tokens

---
+++ database/migrations/2025_02_21_191204_update_token_fields.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->longText('linkedin_access_token')->change();
+            $table->longText('linkedin_refresh_token')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
