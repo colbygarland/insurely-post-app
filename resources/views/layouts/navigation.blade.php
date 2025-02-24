bffceccd1111dc230614730eb3c0a710d2b79efd
Colby Garland <colbygarland@gmail.com>
2025-02-24 12:29:11 -0700

    missed one

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,7 +73,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('posts')" :active="request()->routeIs('posts')">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('posts')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
