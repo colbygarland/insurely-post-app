0aa751bb48ce6dbfd547aa7422d0a350a684ef02
Colby Garland <colbygarland@gmail.com>
2025-06-02 09:01:43 -0600

    remove failsafe check

--- app/Jobs/MakeApiCall.php
+++ app/Jobs/MakeApiCall.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class MakeApiCall implements ShouldQueue
 {
@@ -23,12 +22,6 @@ class MakeApiCall implements ShouldQueue
 
     public function handle()
     {
-        // if (env('OUTBOUND_CALLER_FAILSAFE') == 'true') {
-        Log::info('MakeApiCall: Failsafe is enabled, skipping call');
-
-        return;
-        // }
-
         // "Record ID - Contact","First Name","Last Name","Email","Phone Number"
         $id = $this->data[0];
         $firstName = $this->data[1];
