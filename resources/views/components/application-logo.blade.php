554d304256fd6e26956a8f9de675a5718e66a01b
Colby Garland <colbygarland@gmail.com>
2025-07-20 09:55:07 -0600

    updated the logo

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,4 @@
-<img src="/insight.png" alt="Insight" class="h-6" />
+<div class="flex items-center gap-3">
+    <img src="/insight.png" alt="Insight" class="h-6" />
+    <span class="text-2xl font-bold text-gray-900 dark:text-white">INSIGHT</span>
+</div>
