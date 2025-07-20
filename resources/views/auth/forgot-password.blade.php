2e31c4a435814b7a92f80ca0869774763f7121fb
Colby Garland <colbygarland@gmail.com>
2025-07-20 16:52:19 -0600

    remove password forget for now

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,15 +1,17 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
+    <!-- <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    </div> -->
+    <div class="mb-4 text-sm text-gray-600">
+        {{ __('Forgot your password? No problem. Just let an admin know and they will email you a password reset link that will allow you to choose a new one.') }}
     </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
+    <!-- <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
@@ -21,5 +23,5 @@
                 {{ __('Email Password Reset Link') }}
             </x-primary-button>
         </div>
-    </form>
+    </form> -->
 </x-guest-layout>
