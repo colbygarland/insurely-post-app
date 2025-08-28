7406a41836155df7e9aa5f692211b57bc0336d97
Colby Garland <colbygarland@gmail.com>
2025-08-28 11:59:52 -0600

    added date check

--- app/Http/Controllers/CallLogController.php
+++ app/Http/Controllers/CallLogController.php
@@ -43,6 +43,7 @@ class CallLogController extends Controller
             ->orWhereNull('summary')
             ->orWhereNull('analysis')
             ->where('duration', '<', 900) // 15 minutes maxx time
+            ->where('created_at', '>=', '2025-08-25T17:55:42.000000Z') // after this date generate the transcripts
             ->orderBy('created_at', 'desc')
             ->limit(10)
             ->get();
