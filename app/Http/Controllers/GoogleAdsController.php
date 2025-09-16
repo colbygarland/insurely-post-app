1c908473b5a8f6c8e93264d6765f22e22a9157b9
Colby Garland <colbygarland@gmail.com>
2025-09-16 11:21:22 -0600

    update the google ads controller

--- app/Http/Controllers/GoogleAdsController.php
+++ app/Http/Controllers/GoogleAdsController.php
@@ -9,16 +9,60 @@ class GoogleAdsController extends Controller
 {
     public function webhook(Request $request)
     {
+        Log::info('Google Ads webhook received');
+        Log::info($request->all());
+
         $webhookKey = env('GOOGLE_ADS_WEBHOOK_KEY');
 
-        if ($request->header('X-Goog-Channel-Token') !== $webhookKey) {
+        if ($request->input('google_key') !== $webhookKey) {
             return response()->json(['message' => 'Invalid webhook key'], 401);
         }
 
         // TODO: send the data to the Hubspot integration
-
-        Log::info('Google Ads webhook received');
-        Log::info($request->all());
+        /**
+         * {
+         * "lead_id": "TeSter-123-ABCDEFGHIJKLMNOPQRSTUVWXYZ-abcdefghijklmnopqrstuvwxyz-0123456789-AaBbCcDdEeFfGgHhIiJjKkLl",
+         * "user_column_data": [
+         * {
+         * "column_name": "User Email",
+         * "string_value": "test@example.com",
+         * "column_id": "EMAIL"
+         * },
+         * {
+         * "column_name": "User Phone",
+         * "string_value": "+16505550123",
+         * "column_id": "PHONE_NUMBER"
+         * },
+         * {
+         * "column_name": "Region",
+         * "string_value": "California",
+         * "column_id": "REGION"
+         * },
+         * {
+         * "column_name": "First Name",
+         * "string_value": "FirstName",
+         * "column_id": "FIRST_NAME"
+         *  },
+         * {
+         * "column_name": "Last Name",
+         * "string_value": "LastName",
+         * "column_id": "LAST_NAME"
+         * },
+         * {
+         * "string_value": "Condo",
+         * "column_id": "what_type_of_insurance_are_you_looking_for?"
+         * },
+         * ],
+         * "api_version": "1.0",
+         * "form_id": 194054118492,
+         * "campaign_id": 10000000000,
+         * "google_key": "",
+         * "is_test": true,
+         * "gcl_id": "",
+         * "adgroup_id": 20000000000,
+         * "creative_id": 30000000000
+         * }
+         */
 
         return response()->json(['message' => 'Google Ads webhook received']);
     }
