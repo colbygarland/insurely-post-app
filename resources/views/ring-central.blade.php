0552da72fbe028d45fe8e7cc2ee3b5c6b1325c3d
Colby Garland <colbygarland@gmail.com>
2025-09-30 11:06:52 -0600

    fix the tooltip showing on hover

--- resources/views/ring-central.blade.php
+++ resources/views/ring-central.blade.php
@@ -238,7 +238,7 @@
                                                     <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
                                                         <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/>
                                                     </svg>
-                                                    <div class="absolute text-center bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-900 dark:bg-gray-700 text-white text-xs rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+                                                    <div class="absolute text-center bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-900 dark:bg-gray-700 text-white text-xs rounded whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none">
                                                         Calls less than {{ \App\Http\Controllers\CallLogController::$minimumDuration / 60 }} mins or longer than {{ \App\Http\Controllers\CallLogController::$maximumDuration / 60 }} minutes are not automatically transcribed. 
                                                         <br/>
                                                         You can still manually generate the transcript and analysis! 
