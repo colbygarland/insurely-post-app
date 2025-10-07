9d1ab0932adee41e0213d2df1060757bdec2bbd0
Colby Garland <colbygarland@gmail.com>
2025-10-07 10:24:30 -0600

    remove the call log refresh on home page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,9 +20,6 @@ class DashboardController extends Controller
             return redirect()->route('ringcentral.index');
         }
 
-        // Refresh from ring central
-        RingCentralController::getCallLog();
-
         // Get recent LinkedIn posts (published and pending)
         $recentPublishedPosts = Post::where('published_at', '!=', null)
             ->orderBy('published_at', 'desc')
