99bc4e538b9d4f14cc086c6dcd5ab1d1d372d8fc
Colby Garland <colbygarland@gmail.com>
2025-11-24 10:37:26 -0700

    server side setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,25 +24,13 @@
         </script>
 
         <!-- Scripts -->
+        @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @inertiaHead
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow lg:pt-16">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @inertia
         </div>
     </body>
 </html>
