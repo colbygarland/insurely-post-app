a32781da7d637957f18f583beb4ae7522ae0e9dd
Colby Garland <colbygarland@gmail.com>
2025-11-25 09:50:56 -0700

    Get the sharepoint site ID

--- app/Http/Controllers/MicrosoftController.php
+++ app/Http/Controllers/MicrosoftController.php
@@ -8,6 +8,13 @@ use Illuminate\Support\Facades\Log;
 
 class MicrosoftController extends Controller
 {
+    // The typo is intentional
+    private $HOST_NAME = 'insurley.sharepoint.com';
+
+    private $SITE_PATH = 'sites/InsurelyInc';
+
+    private $MICROSOFT_API_URL = 'https://graph.microsoft.com/v1.0/sites';
+
     /**
      * Used to request admin consent for the Insight app.
      * Needs to be ran by an administrator account in Microsoft Entra.
@@ -38,7 +45,7 @@ class MicrosoftController extends Controller
      * Used to get an access token.
      * https://learn.microsoft.com/en-us/graph/auth-v2-service?tabs=http#step-3-request-an-access-token
      */
-    public function getAccessToken()
+    private function getAccessToken()
     {
         $tenant = env('MICROSOFT_ENTRA_TENANT_ID');
         $response = Http::asForm()->post("https://login.microsoftonline.com/$tenant/oauth2/v2.0/token", [
@@ -58,7 +65,36 @@ class MicrosoftController extends Controller
 
         Log::debug('Success getting access token from Microsoft');
 
-        return $body;
+        return $body['access_token'];
+    }
+
+    /**
+     * Used to get a session
+     * https://learn.microsoft.com/en-us/graph/api/resources/excel?view=graph-rest-1.0#api-call-to-get-a-session
+     */
+    public function getWorkbookSession() {}
+
+    /**
+     * Get the Site ID to then access files or Excel worksheets.
+     * https://learn.microsoft.com/en-us/graph/api/site-get?view=graph-rest-1.0&tabs=http
+     */
+    private function getSharePointSiteID()
+    {
+        Log::debug('getSharePointSiteID(): begin');
+
+        $accessToken = $this->getAccessToken();
+        $url = "$this->MICROSOFT_API_URL/$this->HOST_NAME:/$this->SITE_PATH";
+
+        $siteResponse = Http::withToken($accessToken)
+            ->get($url);
+
+        if ($siteResponse->successful()) {
+            return $siteResponse['id'];
+        }
+
+        Log::error('getSharePointSiteID(): error getting sharepoint site ID');
+
+        return null;
     }
 
     public function callback(Request $request) {}
